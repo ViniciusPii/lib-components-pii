b8fb949cabace815b170ddb0a584ddb7c48ddfdc
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-05-19 20:19:44 -0300

    Fix bug desconstrução do objeto

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Button from './Button';
+import Card from './Card';
+import Container from './Container';
+import Icon from './Icon';
+import Img from './Img';
+import Input from './Input';
+import Layout from './Layout';
+import Text from './Text';
+
+export  {
+  Button,
+  Card,
+  Container,
+  Icon,
+  Img,
+  Input,
+  Layout,
+  Text,
+};
